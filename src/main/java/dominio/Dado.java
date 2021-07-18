72f4950eb5d18d034421e97f02ea01939cf4c9ab
Jorge Blandon <jackronaldo@hotmail.com>
2021-07-18 01:59:50 -0500

    Se dio más aleteoridad al metodo predefinido en la clase dado

--- src/main/java/dominio/Dado.java
+++ src/main/java/dominio/Dado.java
@@ -8,7 +8,7 @@ public class Dado {
     public double lanzarDado () {
         
         double resultadoDado=Math.floor(Math.random()*6+1);
-        double avanceMetros= (resultadoDado*100)/13+Math.random();
+        double avanceMetros=(resultadoDado*100)/13+Math.random();
         return avanceMetros;
     }
     
